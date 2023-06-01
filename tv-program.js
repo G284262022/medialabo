960e72fcc888f1e3990c0822b7803d37b60d15ca
G284262022 <228426@st.takushoku-u.ac.jp>
2023-06-01 15:11:53 +0900

    sample

--- tv-program.js
+++ tv-program.js
@@ -86,6 +86,31 @@ for(let {title} of data.list.g1){
   p1.textContent = title;
 }
 
+let p = document.querySelector('button#request');
+b.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+  let s = document.querySelector('select#service');
+  let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+  let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+  let o = os.item(idx);       // os の idx 番目の要素
+
+  let s1 = document.querySelector('select#service');
+  let idx1 = s.selectedIndex;  // idx 番目の option が選択された
+
+  let os1 = s1.querySelectorAll('option');  // s の子要素 option をすべて検索
+  let o1 = os1.item(idx1);       // os の idx 番目の要素
+
+  console.log('選択された ' + idx + ' 番目の option の情報:');
+  console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
+  console.log('  textContent='+o.textContent);
+
+  console.log('選択された ' + idx1 + ' 番目の option の情報:');
+  console.log('  value=' + o1.getAttribute('value'));  // id 属性を表示
+  console.log('  textContent='+o1.textContent);
+}
+
 let b = document.querySelector('#request');
 b.addEventListener('click', sendRequest);
 
